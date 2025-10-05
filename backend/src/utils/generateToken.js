44dfb7ab890d2f70a8c963b4b622bd0131d850c9
Ashish Rolan <ashishrolan292@gmail.com>
2025-10-05 13:26:53 +0530

    updated jwt

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const generateToken = (_id, res) => {
-  const JWT_SECRET = process.env.JWT_SECRET || "your_default_jwt_secret_key_change_in_production";
+  const JWT_SECRET = process.env.JWT_SECRET || "nillu@5678";
   const token = jwt.sign({ _id }, JWT_SECRET, { expiresIn: "7d" });
 
   // Determine if we're in production
